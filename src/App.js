e7957bc7a4d1e8356605bb972e57011de931eb6b
Menna Hussien <mennaali-v@ejada.com>
2024-11-03 17:08:15 +0200

    init page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CheckIcon from './assets/CheckIcon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', display:'flex', alignItems:'center', flexDirection:'column',
+      justifyContent:'center', height:'100vh'
+     }}>
+      <CheckIcon/>
+      <h1>Your login process has been completed.</h1>
+      <p>You can return back to your application and safely close this window.</p>
     </div>
   );
 }
